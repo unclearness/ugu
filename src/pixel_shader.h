5b53f322584b80b1fbf4759d592dcc1ff0a75d89
unlclearness <example@example.com>
2019-01-24 00:57:48 +0900

    add

---
+++ src/pixel_shader.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2019, unclearness
+ * All rights reserved.
+ */
+
+#pragma once
+
+#include <vector>
+
+#include "include/common.h"
+#include "include/image.h"
+#include "nanort/nanort.h"
+
+namespace crender {
+
+void DefaultShader(crender::Image3b* color, int x, int y,
+                   const nanort::TriangleIntersection<>& isect,
+                   const std::vector<glm::ivec3>& faces,
+                   const std::vector<glm::ivec3>& uv_indices,
+                   const std::vector<glm::vec2>& uv,
+                   const std::vector<glm::vec3>& vertex_colors,
+                   const crender::Image3b& diffuse_texture);
+
+void VertexColorShader(crender::Image3b* color, int x, int y,
+                       const nanort::TriangleIntersection<>& isect,
+                       const std::vector<glm::ivec3>& faces,
+                       const std::vector<glm::ivec3>& uv_indices,
+                       const std::vector<glm::vec2>& uv,
+                       const std::vector<glm::vec3>& vertex_colors,
+                       const crender::Image3b& diffuse_texture);
+
+void DiffuseNnShader(crender::Image3b* color, int x, int y,
+                     const nanort::TriangleIntersection<>& isect,
+                     const std::vector<glm::ivec3>& faces,
+                     const std::vector<glm::ivec3>& uv_indices,
+                     const std::vector<glm::vec2>& uv,
+                     const std::vector<glm::vec3>& vertex_colors,
+                     const crender::Image3b& diffuse_texture);
+
+void DiffuseBilinearShader(crender::Image3b* color, int x, int y,
+                           const nanort::TriangleIntersection<>& isect,
+                           const std::vector<glm::ivec3>& faces,
+                           const std::vector<glm::ivec3>& uv_indices,
+                           const std::vector<glm::vec2>& uv,
+                           const std::vector<glm::vec3>& vertex_colors,
+                           const crender::Image3b& diffuse_texture);
+
+}  // namespace crender
