0e6b3aabad4ae0fcef81ebd123aff680fc754b73
unlclearness <example@example.com>
2019-01-26 11:39:27 +0900

    add timer

--- src/renderer.cc
+++ src/renderer.cc
@@ -7,92 +7,10 @@
 
 #include <array>
 #include <cassert>
-#ifdef _WIN32
-#ifdef __cplusplus
-extern "C" {
-#endif
-#include <windows.h>
-#ifdef __cplusplus
-}
-#endif
-#pragma comment(lib, "winmm.lib")
-#else
-#if defined(__unix__) || defined(__APPLE__)
-#include <sys/time.h>
-#else
-#include <ctime>
-#endif
-#endif
 
 #include "src/pixel_shader.h"
+#include "src/timer.h"
 
-namespace {
-// This class is NOT thread-safe timer!
-class timerutil {
- public:
-#ifdef _WIN32
-  typedef DWORD time_t;
-
-  timerutil() { ::timeBeginPeriod(1); }
-  ~timerutil() { ::timeEndPeriod(1); }
-
-  void start() { t_[0] = ::timeGetTime(); }
-  void end() { t_[1] = ::timeGetTime(); }
-
-  time_t sec() { return (time_t)((t_[1] - t_[0]) / 1000); }
-  time_t msec() { return (time_t)((t_[1] - t_[0])); }
-  time_t usec() { return (time_t)((t_[1] - t_[0]) * 1000); }
-  time_t current() { return ::timeGetTime(); }
-
-#else
-#if defined(__unix__) || defined(__APPLE__)
-  typedef unsigned long int time_t;  // NOLINT
-
-  void start() { gettimeofday(tv + 0, &tz); }
-  void end() { gettimeofday(tv + 1, &tz); }
-
-  time_t sec() { return (time_t)(tv[1].tv_sec - tv[0].tv_sec); }
-  time_t msec() {
-    return this->sec() * 1000 +
-           (time_t)((tv[1].tv_usec - tv[0].tv_usec) / 1000);
-  }
-  time_t usec() {
-    return this->sec() * 1000000 + (time_t)(tv[1].tv_usec - tv[0].tv_usec);
-  }
-  time_t current() {
-    struct timeval t;
-    gettimeofday(&t, NULL);
-    return (time_t)(t.tv_sec * 1000 + t.tv_usec);
-  }
-
-#else  // C timer
-  // using namespace std;
-  typedef clock_t time_t;
-
-  void start() { t_[0] = clock(); }
-  void end() { t_[1] = clock(); }
-
-  time_t sec() { return (time_t)((t_[1] - t_[0]) / CLOCKS_PER_SEC); }
-  time_t msec() { return (time_t)((t_[1] - t_[0]) * 1000 / CLOCKS_PER_SEC); }
-  time_t usec() { return (time_t)((t_[1] - t_[0]) * 1000000 / CLOCKS_PER_SEC); }
-  time_t current() { return (time_t)clock(); }
-
-#endif
-#endif
-
- private:
-#ifdef _WIN32
-  DWORD t_[2];
-#else
-#if defined(__unix__) || defined(__APPLE__)
-  struct timeval tv[2];
-  struct timezone tz;
-#else
-  time_t t_[2];
-#endif
-#endif
-};
-}  // namespace
 
 namespace currender {
 
@@ -158,8 +76,9 @@ bool Renderer::PrepareMesh() {
   LOGI("    # of leaf primitives: %d\n", build_options_.min_leaf_primitives);
   LOGI("    SAH binsize         : %d\n", build_options_.bin_size);
 
-  timerutil t;
-  t.start();
+  Timer<> timer;
+  timer
+      .Start();
 
   triangle_mesh_.reset(new nanort::TriangleMesh<float>(
       &flatten_vertices_[0], &flatten_faces_[0], sizeof(float) * 3));
@@ -179,8 +98,8 @@ bool Renderer::PrepareMesh() {
     return false;
   }
 
-  t.end();
-  LOGI("  BVH build time: %f secs\n", t.msec() / 1000.0);
+  timer.End();
+  LOGI("  BVH build time: %.1f msecs\n", timer.elapsed_msec());
 
   stats_ = accel_.GetStatistics();
 
@@ -242,8 +161,8 @@ bool Renderer::Render(Image3b* color, Image1f* depth, Image1b* mask) const {
     LOGW("No color on geometry\n");
   }
 
-  timerutil time;
-  time.start();
+  Timer<> timer;
+  timer.Start();
 #if defined(_OPENMP) && defined(CURRENDER_USE_OPENMP)
 #pragma omp parallel for schedule(dynamic, 1)
 #endif
@@ -309,8 +228,8 @@ bool Renderer::Render(Image3b* color, Image1f* depth, Image1b* mask) const {
     }
   }
 
-  time.end();
-  LOGI("  Rendering main loop time: %f secs\n", time.msec() / 1000.0);
+  timer.End();
+  LOGI("  Rendering main loop time: %.1f msecs\n", timer.elapsed_msec());
 
   return true;
 }
