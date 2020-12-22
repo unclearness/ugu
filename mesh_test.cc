425002ac8797805535fd2b86a03b07325a53f162
unlclearness <example@example.com>
2020-12-23 00:14:04 +0900

    Add TestBlendshapes and data Enable c++17 for filesystem

--- mesh_test.cc
+++ mesh_test.cc
@@ -5,6 +5,106 @@
 
 #include "ugu/mesh.h"
 
+#include <filesystem>
+#include <iostream>
+
+inline std::vector<std::string> Split(const std::string& s, char delim) {
+  std::vector<std::string> elems;
+  std::stringstream ss(s);
+  std::string item;
+  while (std::getline(ss, item, delim)) {
+    if (!item.empty()) {
+      elems.push_back(item);
+    }
+  }
+  return elems;
+}
+
+inline bool GetFileNames(std::string folderPath,
+                         std::vector<std::string>& file_names) {
+  using namespace std::filesystem;
+  directory_iterator iter(folderPath), end;
+  std::error_code err;
+
+  for (; iter != end && !err; iter.increment(err)) {
+    const directory_entry entry = *iter;
+    std::string name = *(Split(entry.path().string(), '/').end() - 1);
+    file_names.push_back(name);
+  }
+
+  if (err) {
+    std::cout << err.value() << std::endl;
+    std::cout << err.message() << std::endl;
+    return false;
+  }
+  return true;
+}
+
+inline std::string ExtractPathExt(const std::string& fn) {
+  std::string::size_type pos;
+  if ((pos = fn.find_last_of(".")) == std::string::npos) {
+    return "";
+  }
+  return fn.substr(pos + 1, fn.size());
+}
+
+inline std::string ExtractPathWithoutExt(const std::string& fn) {
+  std::string::size_type pos;
+  if ((pos = fn.find_last_of(".")) == std::string::npos) {
+    return fn;
+  }
+
+  return fn.substr(0, pos);
+}
+
+void TestBlendshapes() {
+  std::string data_dir = "../data/blendshape/";
+  std::vector<std::string> file_names;
+  GetFileNames(data_dir, file_names);
+  std::string base_name = "cube.obj";
+  ugu::Mesh base_mesh;
+  base_mesh.LoadObj(data_dir + base_name, data_dir);
+  base_mesh.SplitMultipleUvVertices();
+
+  std::vector<std::string> obj_names;
+  for (auto& name : file_names) {
+    auto ext = ExtractPathExt(name);
+    if (name == base_name || ext != "obj") {
+      continue;
+    }
+    obj_names.push_back(name);
+  }
+
+  std::vector<ugu::Mesh> blendshape_meshes;
+  for (auto& name : obj_names) {
+    ugu::Mesh mesh;
+    mesh.LoadObj(data_dir + name, data_dir);
+    mesh.SplitMultipleUvVertices();
+    blendshape_meshes.push_back(mesh);
+  }
+
+  std::vector<ugu::Blendshape> blendshapes;
+  for (int i = 0; i < blendshape_meshes.size(); i++) {
+    auto& blendshape_mesh = blendshape_meshes[i];
+    auto name = ExtractPathWithoutExt(obj_names[i]);
+
+    ugu::Blendshape blendshape;
+    std::vector<Eigen::Vector3f> displacement = base_mesh.vertices();
+    for (size_t j = 0; j < displacement.size(); j++) {
+      displacement[j] = blendshape_mesh.vertices()[j] - displacement[j];
+    }
+    blendshape.vertices = displacement;
+    blendshape.normals = blendshape_mesh.normals();
+    blendshape.name = name;
+
+    blendshapes.push_back(blendshape);
+  }
+  base_mesh.set_blendshapes(blendshapes);
+
+  base_mesh.WriteGltfSeparate(data_dir, "blendshape");
+  base_mesh.WriteGlb(data_dir, "blendshape.glb");
+}
+
 void TestIO() {
   std::string data_dir = "../data/bunny/";
   std::string in_obj_path = data_dir + "bunny.obj";
@@ -39,6 +139,9 @@ void TestMerge() {
   ugu::Mesh buddha;
   buddha.LoadObj(in_obj_path2, data2_dir);
 
+  buddha.SplitMultipleUvVertices();
+  buddha.WriteGlb(data2_dir, "buddha.glb");
+
   ugu::MergeMeshes(bunny, buddha, &dst);
   dst.WriteObj(data1_dir, "bunny_and_buddha");
 
@@ -72,6 +175,8 @@ void TestRemove() {
 }
 
 int main() {
+  TestBlendshapes();
+
   TestIO();
 
   TestMerge();
