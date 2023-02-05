1b0c0fb93c404314a91952e6dcfb11dbb85c6fbc
unlclearness <unclearness.dev@gmail.com>
2023-02-05 14:43:44 +0900

    Add translation by drag with middle buttion

--- app_gui/mesh_viewer/main.cc
+++ app_gui/mesh_viewer/main.cc
@@ -1,5 +1,3 @@
-#if 1
-
 // Dear ImGui: standalone example application for GLFW + OpenGL 3, using
 // programmable pipeline (GLFW is a cross-platform general purpose library for
 // handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation,
@@ -102,14 +100,19 @@ Eigen::Vector2d g_prev_cursor_pos;
 Eigen::Vector2d g_cursor_pos;
 Eigen::Vector2d g_mouse_l_pressed_pos;
 Eigen::Vector2d g_mouse_l_released_pos;
+Eigen::Vector2d g_mouse_m_pressed_pos;
+Eigen::Vector2d g_mouse_m_released_pos;
 // Eigen::Vector3d g_center;
 // ugu::MeshStats g_stats;
 Eigen::Vector3f g_bb_max, g_bb_min;
 CameraPtr g_camera;
-bool g_to_process_drag = false;
+bool g_to_process_drag_l = false;
+bool g_to_process_drag_m = false;
 
 bool g_prev_mouse_l_pressed = false;
+bool g_prev_mouse_m_pressed = false;
 bool g_mouse_l_pressed = false;
+bool g_mouse_m_pressed = false;
 const double drag_th = 2.0;
 
 double g_mouse_wheel_yoffset = 0.0;
@@ -128,16 +131,27 @@ void mouse_button_callback(GLFWwindow *pwin, int button, int action, int mods) {
       g_mouse_l_released_pos = g_cursor_pos;
 
       if ((g_mouse_l_pressed_pos - g_mouse_l_released_pos).norm() > drag_th) {
-        std::cout << "drag finish " << g_mouse_l_pressed_pos << " -> "
-                  << g_mouse_l_released_pos << std::endl;
+        // std::cout << "drag finish " << g_mouse_l_pressed_pos << " -> "
+        //           << g_mouse_l_released_pos << std::endl;
       }
     }
   }
   if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) {
     // printf("R - down\n");
   }
-  if (button == GLFW_MOUSE_BUTTON_MIDDLE && action == GLFW_PRESS) {
-    //  printf("M - down\n");
+  if (button == GLFW_MOUSE_BUTTON_MIDDLE) {
+    g_prev_mouse_m_pressed = g_mouse_m_pressed;
+    g_mouse_m_pressed = action == GLFW_PRESS;
+    if (g_mouse_m_pressed) {
+      g_mouse_m_pressed_pos = g_cursor_pos;
+    } else {
+      g_mouse_m_released_pos = g_cursor_pos;
+
+      if ((g_mouse_m_pressed_pos - g_mouse_m_released_pos).norm() > drag_th) {
+        // std::cout << "drag finish " << g_mouse_m_pressed_pos << " -> "
+        //           << g_mouse_m_released_pos << std::endl;
+      }
+    }
   }
 }
 
@@ -161,7 +175,11 @@ void cursor_pos_callback(GLFWwindow *window, double xoffset, double yoffset) {
   g_cursor_pos[1] = yoffset;
   if (g_mouse_l_pressed) {
     // std::cout << "dragging..." << std::endl;
-    g_to_process_drag = true;
+    g_to_process_drag_l = true;
+  }
+  if (g_mouse_m_pressed) {
+    // std::cout << "dragging..." << std::endl;
+    g_to_process_drag_m = true;
   }
 }
 
@@ -179,6 +197,7 @@ void drop_callback(GLFWwindow *window, int count, const char **paths) {
     mesh->LoadObj(obj_path, obj_dir);
     meshes.push_back(mesh);
   } else {
+    return;
   }
 
   g_bb_max.setConstant(std::numeric_limits<float>::lowest());
@@ -202,7 +221,6 @@ void drop_callback(GLFWwindow *window, int count, const char **paths) {
     g_renderer->SetMesh(mesh);
   }
   g_renderer->Init();
-
 }
 
 }  // namespace
@@ -504,10 +522,9 @@ int main(int, char **) {
 
     g_renderer->Draw();
 
-    if (g_mouse_l_pressed && g_to_process_drag) {
-      g_to_process_drag = false;
+    if (g_to_process_drag_l) {
+      g_to_process_drag_l = false;
       Eigen::Vector2d diff = g_cursor_pos - g_prev_cursor_pos;
-      // std::cout << g_prev_cursor_pos << " -> " << g_cursor_pos << std::endl;
       if (diff.norm() > drag_th) {
         const double rotate_speed = ugu::pi / 180 * 10;
 
@@ -516,10 +533,6 @@ int main(int, char **) {
 
         Eigen::Vector3d right_axis = -R_cur.col(0);
         Eigen::Vector3d up_axis = -R_cur.col(1);
-
-        // Eigen::Matrix3d R_new =
-        //     Eigen::AngleAxisd(2 * ugu::pi * diff[0], right_axis) *
-        //     Eigen::AngleAxisd(2 * ugu::pi * diff[1], up_axis) * R_cur;
 
         Eigen::Quaterniond R_offset =
             Eigen::AngleAxisd(2 * ugu::pi * diff[0] / height * rotate_speed,
@@ -530,22 +543,32 @@ int main(int, char **) {
         Eigen::Affine3d cam_pose_new = R_offset * cam_pose_cur;
 
         g_camera->set_c2w(cam_pose_new);
-
-        // std::cout << cam_pose_cur.translation() << " -> "
-        //           << cam_pose_new.translation() << std::endl;
-
-#if 0
-      rotateDelta.subVectors(rotateEnd, rotateStart)
-          .multiplyScalar(scope.rotateSpeed);
-      const element = scope.domElement;
-      rotateLeft(2 * Math.PI * rotateDelta.x /
-                 element.clientHeight);  // yes, height
-
-      rotateUp(2 * Math.PI * rotateDelta.y / element.clientHeight);
-      rotateStart.copy(rotateEnd);
-#endif
       }
     }
+
+    if (g_to_process_drag_m) {
+      g_to_process_drag_m = false;
+      Eigen::Vector2d diff = g_cursor_pos - g_prev_cursor_pos;
+      if (diff.norm() > drag_th) {
+        const double trans_speed =
+            (g_bb_max - g_bb_min).maxCoeff() / height;
+
+        Eigen::Affine3d cam_pose_cur = g_camera->c2w();
+        Eigen::Matrix3d R_cur = cam_pose_cur.rotation();
+
+        Eigen::Vector3d right_axis = -R_cur.col(0);
+        Eigen::Vector3d up_axis = R_cur.col(1);
+
+        Eigen::Vector3d t_offset = right_axis * diff[0] * trans_speed +
+                                   up_axis * diff[1] * trans_speed;
+
+        Eigen::Affine3d cam_pose_new =
+            Eigen::Translation3d(t_offset + cam_pose_cur.translation()) *
+            cam_pose_cur.rotation();
+        g_camera->set_c2w(cam_pose_new);
+      }
+    }
+
     if (g_to_process_wheel) {
       g_to_process_wheel = false;
       const double wheel_speed = (g_bb_max - g_bb_min).maxCoeff() / 20;
@@ -553,7 +576,6 @@ int main(int, char **) {
       Eigen::Affine3d cam_pose_cur = g_camera->c2w();
       Eigen::Vector3d t_offset =
           cam_pose_cur.rotation().col(2) * -g_mouse_wheel_yoffset * wheel_speed;
-      std::cout << t_offset << std::endl;
       Eigen::Affine3d cam_pose_new =
           Eigen::Translation3d(t_offset + cam_pose_cur.translation()) *
           cam_pose_cur.rotation();
@@ -623,214 +645,3 @@ int main(int, char **) {
 
   return 0;
 }
-#else
-
-#include <GLFW/glfw3.h>
-
-#include <iostream>
-
-#include "glad/gl.h"
-
-void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-void processInput(GLFWwindow *window);
-
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
-const char *vertexShaderSource =
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-const char *fragmentShaderSource =
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
-
-int main() {
-  // glfw: initialize and configure
-  // ------------------------------
-  glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-  // glfw window creation
-  // --------------------
-  GLFWwindow *window =
-      glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-  if (window == NULL) {
-    std::cout << "Failed to create GLFW window" << std::endl;
-    glfwTerminate();
-    return -1;
-  }
-  glfwMakeContextCurrent(window);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-  // glad: load all OpenGL function pointers
-  // ---------------------------------------
-  // if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-  if (!gladLoadGL(glfwGetProcAddress)) {
-    std::cout << "Failed to initialize GLAD" << std::endl;
-    return -1;
-  }
-
-  // build and compile our shader program
-  // ------------------------------------
-  // vertex shader
-  unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-  glCompileShader(vertexShader);
-  // check for shader compile errors
-  int success;
-  char infoLog[512];
-  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-  if (!success) {
-    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
-              << infoLog << std::endl;
-  }
-  // fragment shader
-  unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-  glCompileShader(fragmentShader);
-  // check for shader compile errors
-  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-  if (!success) {
-    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
-              << infoLog << std::endl;
-  }
-  // link shaders
-  unsigned int shaderProgram = glCreateProgram();
-  glAttachShader(shaderProgram, vertexShader);
-  glAttachShader(shaderProgram, fragmentShader);
-  glLinkProgram(shaderProgram);
-  // check for linking errors
-  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-  if (!success) {
-    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-    std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
-              << infoLog << std::endl;
-  }
-  glDeleteShader(vertexShader);
-  glDeleteShader(fragmentShader);
-
-  // set up vertex data (and buffer(s)) and configure vertex attributes
-  // ------------------------------------------------------------------
-  float vertices[] = {
-      0.5f,  0.5f,  0.0f,  // top right
-      0.5f,  -0.5f, 0.0f,  // bottom right
-      -0.5f, -0.5f, 0.0f,  // bottom left
-      -0.5f, 0.5f,  0.0f   // top left
-  };
-  unsigned int indices[] = {
-      // note that we start from 0!
-      0, 1, 3,  // first Triangle
-      1, 2, 3   // second Triangle
-  };
-  unsigned int VBO, VAO, EBO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
-  glGenBuffers(1, &EBO);
-  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and
-  // then configure vertex attributes(s).
-  glBindVertexArray(VAO);
-
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-               GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(0);
-
-  // note that this is allowed, the call to glVertexAttribPointer registered VBO
-  // as the vertex attribute's bound vertex buffer object so afterwards we can
-  // safely unbind
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-  // remember: do NOT unbind the EBO while a VAO is active as the bound element
-  // buffer object IS stored in the VAO; keep the EBO bound.
-  // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-  // You can unbind the VAO afterwards so other VAO calls won't accidentally
-  // modify this VAO, but this rarely happens. Modifying other VAOs requires a
-  // call to glBindVertexArray anyways so we generally don't unbind VAOs (nor
-  // VBOs) when it's not directly necessary.
-  glBindVertexArray(0);
-
-  // uncomment this call to draw in wireframe polygons.
-  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-  // render loop
-  // -----------
-  while (!glfwWindowShouldClose(window)) {
-    // input
-    // -----
-    processInput(window);
-
-    // render
-    // ------
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // draw our first triangle
-    glUseProgram(shaderProgram);
-    glBindVertexArray(VAO);  // seeing as we only have a single VAO there's no
-                             // need to bind it every time, but we'll do so to
-                             // keep things a bit more organized
-    // glDrawArrays(GL_TRIANGLES, 0, 6);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    // glBindVertexArray(0); // no need to unbind it every time
-
-    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
-    // etc.)
-    // -------------------------------------------------------------------------------
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
-
-  // optional: de-allocate all resources once they've outlived their purpose:
-  // ------------------------------------------------------------------------
-  glDeleteVertexArrays(1, &VAO);
-  glDeleteBuffers(1, &VBO);
-  glDeleteBuffers(1, &EBO);
-  glDeleteProgram(shaderProgram);
-
-  // glfw: terminate, clearing all previously allocated GLFW resources.
-  // ------------------------------------------------------------------
-  glfwTerminate();
-  return 0;
-}
-
-// process all input: query GLFW whether relevant keys are pressed/released this
-// frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, true);
-}
-
-// glfw: whenever the window size changed (by OS or user resize) this callback
-// function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-  // make sure the viewport matches the new window dimensions; note that width
-  // and height will be significantly larger than specified on retina displays.
-  glViewport(0, 0, width, height);
-}
-
-#endif
