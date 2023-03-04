f542a21377dde74944d18337c921fdcd06551f5c
unlclearness <unclearness.dev@gmail.com>
2023-03-04 10:52:14 +0900

    WIP

--- include/ugu/renderer/gl/renderer.h
+++ include/ugu/renderer/gl/renderer.h
@@ -18,6 +18,37 @@
 
 namespace ugu {
 
+/// Holds all state information relevant to a character as loaded using FreeType
+struct Character {
+  unsigned int TextureID;  // ID handle of the glyph texture
+  Eigen::Vector2i Size;         // size of glyph
+  Eigen::Vector2i Bearing;  // offset from baseline to left/top of glyph
+  unsigned int Advance;    // horizontal offset to advance to next glyph
+};
+
+// A renderer class for rendering text displayed by a font loaded using the
+// FreeType library. A single font is loaded, processed into a list of Character
+// items for later rendering.
+class TextRenderer {
+ public:
+  // holds a list of pre-compiled Characters
+  std::map<char, Character> Characters;
+  // shader used for text rendering
+  Shader TextShader;
+  // constructor
+  TextRenderer(unsigned int width, unsigned int height);
+  // pre-compiles a list of characters from the given font
+  void Load(std::string font, unsigned int fontSize);
+  // renders a string of text using the precompiled list of characters
+  void RenderText(std::string text, float x, float y, float scale,
+                  Eigen::Vector3f color = Eigen::Vector3f(1.0f));
+
+ private:
+  // render state
+  unsigned int VAO, VBO;
+};
+
+
 class RendererGl {
  public:
   RendererGl();
