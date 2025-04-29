ecf5f08e27f283183fcc5a1c1635cdc00292862e
unlclearness <unclearness.dev@gmail.com>
2025-04-29 22:55:16 +0900

    Fix

--- reconfigure.bat
+++ reconfigure.bat
@@ -9,13 +9,13 @@ cd win_build
 IF EXIST CMakeCache.txt (
     del CMakeCache.txt
 )
-
+rem  NOTE: For freetype, -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 IF EXIST "C:\\Program Files\\Microsoft Visual Studio\\2022\\" (
-    cmake -G "Visual Studio 17 2022" ..
+    cmake -G "Visual Studio 17 2022" -DCMAKE_POLICY_VERSION_MINIMUM=3.5  ..
 ) ELSE IF EXIST "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\" (
-    cmake -G "Visual Studio 16 2019" ..
+    cmake -G "Visual Studio 16 2019" -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ..
 ) ELSE IF EXIST "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\" (
-    cmake -G "Visual Studio 15 2017 Win64" ..
+    cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ..
 )
 
 cd ..
